794ad6c5562b96123c13a127b795eb79a751ac1b
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-19 06:40:44 +0800

    Using linter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
-import MainLayout from "./Layouts/MainLayout.vue";
+import MainLayout from "@/Layouts/MainLayout.vue";
 
 createInertiaApp({
-    resolve: (name) => {
+    resolve:async (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        const page= pages[`./Pages/${name}.vue`];
+        const page=await pages[`./Pages/${name}.vue`];
         page.default.layout=page.default.layout || MainLayout //set defult layout by MainLayout If layout not set on page
         return page
     },
