d83a2e2031a09784038b332948871926b42d2221
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-18 22:06:50 +0800

    Creating first pages

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Index</h1>
+        <Link href="/hello">Show Page</Link>
+    </div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3'
+</script>
