2b0c67349756401ec863857eae31f2d9be849c80
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-19 16:24:57 +0800

    Computed Data and Conditional Rendering (Displaying Flash Messages)

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,25 @@
 <template>
     <Link href="/">Main Page</Link>
     <Link href="/hello">Show Page</Link>
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
 
-    
     <slot>Default</slot>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/vue3'
+import {Link, usePage}  from '@inertiajs/vue3'
+import { computed } from 'vue'
+const page = usePage()
+const flashSuccess = computed(
+    () => page.props.flash.success
+)
 </script>
+
+<style scoped>
+    .success{
+        background-color: green;
+        color: white;
+    }
+</style>
