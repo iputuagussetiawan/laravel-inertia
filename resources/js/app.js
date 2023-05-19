0ecebdfa5fc1fa89b26af236cf71120e6970e588
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-20 05:55:21 +0800

    Progress Bar Loading Indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,44 @@
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
-import { ZiggyVue } from 'ziggy'
-import '../css/app.css'
+import { ZiggyVue } from "ziggy";
+import "../css/app.css";
+import NProgress from "nprogress";
+import { router } from "@inertiajs/vue3";
+
+let timeout = null;
+
+router.on("start", () => {
+    timeout = setTimeout(() => NProgress.start(), 250);
+});
+
+router.on("progress", (event) => {
+    if (NProgress.isStarted() && event.detail.progress.percentage) {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.9);
+    }
+});
+
+router.on("finish", (event) => {
+    clearTimeout(timeout);
+    if (!NProgress.isStarted()) {
+        return;
+    } else if (event.detail.visit.completed) {
+        NProgress.done();
+    } else if (event.detail.visit.interrupted) {
+        NProgress.set(0);
+    } else if (event.detail.visit.cancelled) {
+        NProgress.done();
+        NProgress.remove();
+    }
+});
 
 createInertiaApp({
-    resolve:async (name) => {
+    progress: false,
+    resolve: async (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        const page=await pages[`./Pages/${name}.vue`];
-        page.default.layout=page.default.layout || MainLayout //set defult layout by MainLayout If layout not set on page
-        return page
+        const page = await pages[`./Pages/${name}.vue`];
+        page.default.layout = page.default.layout || MainLayout; //set defult layout by MainLayout If layout not set on page
+        return page;
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
