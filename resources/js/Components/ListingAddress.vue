c5eb3318ce320f91784a2cdcc94088a3e85041bf
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-19 15:07:12 +0800

    Vue Directives (v-for, v-bind) & Custom Components

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,15 @@
+<template>
+    <span>
+        {{ listing.street }}
+        {{ listing.street_nr }}
+        {{ listing.city}},For
+        {{ listing.price }}
+    </span>
+</template>
+
+
+<script setup>
+    defineProps({
+        listing:Object
+    })
+</script>
