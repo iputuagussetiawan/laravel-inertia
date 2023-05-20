90164281f4356adae98e0ed8ea1044bd43d8261f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-20 08:07:46 +0800

    Payment Calculator Markup (Sliders)

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -15,9 +15,22 @@
                 </Box>
                 <Box>
                     <template #header>
-                        Offer
+                        Monthly Payment
                     </template>
-                    Make an offer
+                    <div>
+                        <label class="label">Interest rate (2.5%)</label>
+                        <input type="range" min="0.1" max="30" step="0.1"
+                            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                        <label class="label">Duration (25 years)</label>
+                        <input type="range" min="3" max="35" step="1"
+                            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+
+                        <div class="text-gray-600 dark:text-gray-300 mt-2">
+                            <div class="text-gray-400">Your monthly payment</div>
+                            <Price :price="500" class="text-3xl" />
+                        </div>
+                    </div>
                 </Box>
             </div>
         </div>
@@ -25,11 +38,10 @@
 </template>
 
 <script setup>
-    import ListingAddress from '@/Components/ListingAddress.vue'
-    import ListingSpace from '@/Components/ListingSpace.vue'
-    import Price from '@/Components/Price.vue'
-    import Box from '../../Components/UI/Box.vue'
-    defineProps({
-        listing: Object
-    })
+import ListingAddress from '@/Components/ListingAddress.vue'
+import ListingSpace from '@/Components/ListingSpace.vue'
+import Box from '@/Components/UI/Box.vue'
+defineProps({
+    listing: Object
+})
 </script>
