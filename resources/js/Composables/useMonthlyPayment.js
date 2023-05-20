27a905f9ea9795096147340a33108ddafb19eb1d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-20 09:39:54 +0800

    Adding More Composable Members (Total Paid, Total Interest)

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -4,7 +4,7 @@ export const useMonthlyPayment = (total, interestRate, duration) => {
     const monthlyPayment = computed(() => {
         const principle = isRef(total) ? total.value : total;
         const monthlyInterest =
-            (isRef(interestRate) ? interestRate.value : interestRate) /100 /12;
+            (isRef(interestRate) ? interestRate.value : interestRate) / 100 / 12;
         const numberOfPaymentMonths =
             (isRef(duration) ? duration.value : duration) * 12;
 
@@ -12,5 +12,11 @@ export const useMonthlyPayment = (total, interestRate, duration) => {
             (principle * monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) / (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
         );
     });
-    return { monthlyPayment };
+    const totalPaid = computed(() => {
+        return (isRef(duration) ? duration.value : duration) * 12 * monthlyPayment.value
+    })
+
+    const totalInterest = computed(() => totalPaid.value - (isRef(total) ? total.value : total))
+
+    return { monthlyPayment, totalPaid, totalInterest }
 };
