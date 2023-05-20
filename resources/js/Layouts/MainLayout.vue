d5e076c8ed7f9608f5dc434bba743169907d0fdd
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-20 11:41:35 +0800

    Getting the Currently Logged In User

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -8,9 +8,17 @@
                 <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
                     <Link :href="route('listing.index')">LaraProperties</Link>
                 </div>
-                <div>
+                
+                <div v-if="user" class="flex items-center gap-4">
+                    <div class="text-sm text-gray-500">{{ user.name }}</div>
+                    <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+                    <div>Logout</div>
+                </div>
+                <div v-else>
+                    <Link :href="route('login')">Sign-In</Link>
                     <Link :href="route('listing.create')"
-                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing
+                    </Link>
                 </div>
             </nav>
         </div>
@@ -30,6 +38,9 @@ import { computed } from 'vue'
 const page = usePage()
 const flashSuccess = computed(
     () => page.props.flash.success
+)
+const user = computed(
+    () => page.props.user,
 )
 </script>
 
