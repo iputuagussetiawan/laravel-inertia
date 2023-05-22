7f6d954b2c264a1ff92df77285cca35b96fe6cee
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-22 21:29:21 +0800

    Styling the Upload Input & UX Improvement

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -2,31 +2,42 @@
     <Box>
         <template #header>Upload New Images</template>
         <form @submit.prevent="upload">
-            <input type="file" multiple @input="addFiles" />
-            <button type="submit" class="btn-outline">Upload</button>
-            <button type="reset" class="btn-outline" @click="reset">Reset</button>
+            <section class="flex items-center gap-2 my-4">
+                <input
+                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+                    type="file" multiple @input="addFiles" />
+                <button type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
+                    :disabled="!canUpload">
+                    Upload
+                </button>
+                <button type="reset" class="btn-outline" @click="reset">
+                    Reset
+                </button>
+            </section>
         </form>
     </Box>
 </template>
 <script setup>
-    import Box from '@/Components/UI/Box.vue'
-    import { useForm } from '@inertiajs/vue3'
-    const props = defineProps({ listing: Object })
-    const form = useForm({
-        images: [],
-    })
-    const upload = () => {
-        form.post(
-            route('realtor.listing.image.store', { listing: props.listing.id }),
-            {
-                onSuccess: () => form.reset('images'),
-            },
-        )
+import { computed } from 'vue'
+import Box from '@/Components/UI/Box.vue'
+import { useForm } from '@inertiajs/vue3'
+const props = defineProps({ listing: Object })
+const form = useForm({
+    images: [],
+})
+const canUpload = computed(() => form.images.length)
+const upload = () => {
+    form.post(
+        route('realtor.listing.image.store', { listing: props.listing.id }),
+        {
+            onSuccess: () => form.reset('images'),
+        },
+    )
+}
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image)
     }
-    const addFiles = (event) => {
-        for (const image of event.target.files) {
-            form.images.push(image)
-        }
-    }
-    const reset = () => form.reset('images')
+}
+const reset = () => form.reset('images')
 </script>
