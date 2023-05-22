b7b9c79dc45210c1d6845027bce57e82c6beb72d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2023-05-22 21:01:12 +0800

    Uploading Multiple Images (Backend Part)

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,9 +1,10 @@
 <template>
-    <form method="POST" :action="route('realtor.listing.image.store', { listing: listing.id })">
-        <input type="file" multiple name="files" />
+    <form enctype="multipart/form-data" method="POST"
+        :action="route('realtor.listing.image.store', { listing: listing.id })">
+        <input type="file" multiple name="images[]" />
         <button type="submit">Send</button>
     </form>
 </template>
 <script setup>
-    defineProps({ listing: Object })
+defineProps({ listing: Object })
 </script>
